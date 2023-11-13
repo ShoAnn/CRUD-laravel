ada2534769248a7a916fc25637df9d8e8522fd24
M. Shokhibul Anwar <73224918+ShoAnn@users.noreply.github.com>
2023-11-13 20:26:53 +0700

    Updated dashboard Added show product page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.static.home');
 });
+
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
