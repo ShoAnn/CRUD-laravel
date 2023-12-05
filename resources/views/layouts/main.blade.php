6c987c9785b412a31ef39367e89abfcda434c071
M. Shokhibul Anwar <73224918+ShoAnn@users.noreply.github.com>
2023-12-05 20:17:58 +0700

    small fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,63 +16,12 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('template/dist/css/adminlte.min.css') }}">
     {{-- Highcharts --}}
-    <style>
-        @import url("{{ asset('vendor/highcharts/highcharts.css') }}");
-
-        .highcharts-pie-series .highcharts-point {
-            stroke: #ede;
-            stroke-width: 2px;
-        }
-
-        .highcharts-pie-series .highcharts-data-label-connector {
-            stroke: silver;
-            stroke-dasharray: 2, 2;
-            stroke-width: 2px;
-        }
-
-        .highcharts-figure,
-        .highcharts-data-table table {
-            min-width: 220px;
-            max-width: auto;
-            margin: 1em auto;
-        }
-
-        .highcharts-data-table table {
-            font-family: Verdana, sans-serif;
-            border-collapse: collapse;
-            border: 1px solid #ebebeb;
-            margin: 10px auto;
-            text-align: center;
-            width: 100%;
-            max-width: 500px;
-        }
-
-        .highcharts-data-table caption {
-            padding: 1em 0;
-            font-size: 1.2em;
-            color: #ffffff;
-        }
-
-        .highcharts-data-table th {
-            font-weight: 400;
-            padding: 0.5em;
-        }
-
-        .highcharts-data-table td,
-        .highcharts-data-table th,
-        .highcharts-data-table caption {
-            padding: 0.5em;
-        }
-
-        .highcharts-data-table thead tr,
-        .highcharts-data-table tr:nth-child(even) {
-            background: #353535;
-        }
-    </style>
+    @yield('header')
 
 </head>
 
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<body data-overlayscrollbars-initialize
+    class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div class="wrapper">
         @include('sweetalert::alert')
         @include('layouts.partials.navbar')
@@ -87,41 +36,15 @@
 
     <!-- REQUIRED SCRIPTS -->
     <!-- jQuery -->
-    <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/jquery/jquery.min.js') }}" defer></script>
     <!-- Bootstrap -->
-    <script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}" defer></script>
     <!-- overlayScrollbars -->
-    <script src="{{ asset('template/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+    <script src="{{ asset('template/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}" defer></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('template/dist/js/adminlte.js') }}"></script>
+    <script src="{{ asset('template/dist/js/adminlte.js') }}" defer></script>
 
-    {{-- Highcharts --}}
-    <script src="{{ asset('vendor/highcharts/highcharts.js') }}"></script>
-    <script src="{{ asset('vendor/highcharts/exporting.js') }}"></script>
-    <script src="{{ asset('vendor/highcharts/export-data.js') }}"></script>
-    <script src="{{ asset('vendor/highcharts/accessibility.js') }}"></script>
-    @yield('chart')
-
-    <!-- PAGE PLUGINS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
-    <script type="text/javascript">
-        $('.show_confirm').click(function(event) {
-            var form = $(this).closest("form");
-            var name = $(this).data("name");
-            event.preventDefault();
-            swal({
-                    title: `Image will be deleted`,
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                .then((willDelete) => {
-                    if (willDelete) {
-                        form.submit();
-                    }
-                });
-        });
-    </script>
+    @yield('script')
 </body>
 
 </html>
